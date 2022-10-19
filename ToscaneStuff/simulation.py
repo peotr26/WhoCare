d56b09cf6cabaee4b4c2c1f0f940f6b2553a7664
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-10-19 17:27:43 +0200

    Update simulation.py

--- ToscaneStuff/simulation.py
+++ ToscaneStuff/simulation.py
@@ -10,8 +10,6 @@ def simulation(og):
     num_list = []
     for i in range(0, og):
         num_list.append(lancer())
-    count_10 = counting(10, num_list)
-    count_9 = counting(9, num_list)
-    return count_10, count_9
+    return counting(10, num_list), counting(9, num_list)
 
 print(simulation(100000))
