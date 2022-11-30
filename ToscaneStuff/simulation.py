b01def82601e997b178773583ca67c5c9db03f51
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-11-30 17:55:40 +0100

    Adding documentation for ToscaneStuff
    I added documentation for ToscaneStuff as well as cleaning up some code. You are now expected to import the script in a Python console to run the script.

--- ToscaneStuff/simulation.py
+++ ToscaneStuff/simulation.py
@@ -1,15 +1,26 @@
+# Importing libraries:
 from random import randint
 
-def lancer():
+# Functions:
+def throw()->int:
+    '''This function generates a three dices throw.
+    The integer that is return is the sum of the three dices result.
+    '''
     return randint(1, 6) + randint(1, 6) + randint(1, 6)
 
-def counting(n, list):
+def counting(n:int, list:list)->int:
+    '''This function counts the number of occurence of an integer in a specified list.
+
+    n -- The integer for which the function is going to count the number of occurences.
+    list -- The list in which the number of occurence of n is going to be counted.
+    '''
     return list.count(n)
 
-def simulation(og):
-    num_list = []
-    for i in range(0, og):
-        num_list.append(lancer())
-    return counting(10, num_list), counting(9, num_list)
+# Main function of the script:
+def simulation(mag:int)->tuple:
+    '''This is the main function of the script.
 
-print(simulation(100000))
+    mag -- The integer that specifies the number of throws for the final result.
+    '''
+    num_list = [throw() for i in range(0, mag)]
+    return counting(10, num_list), counting(9, num_list)
